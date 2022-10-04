b702e8dbaf8a5a4b10b76cf944c02724382d26e3
hayelee <ezezezi94@gmail.com>
2022-10-04 17:40:37 +0900

    20221004 HighJava

--- JavaThreadTest/src/kr/or/ddit/basic/T20WaitNotifyTest.java
+++ JavaThreadTest/src/kr/or/ddit/basic/T20WaitNotifyTest.java
@@ -2,38 +2,38 @@ package kr.or.ddit.basic;
 
 public class T20WaitNotifyTest {
 	public static void main(String[] args) {
-		
+
 		DataBox dataBox = new DataBox();
-		
+
 		ProducerThread pTh = new ProducerThread(dataBox);
 		ConsumerThread cTh = new ConsumerThread(dataBox);
-		
+
 		pTh.start();
 		cTh.start();
-		
+
 	}
 }
 
 // 데이터를 공통으로 사용하는 클래스(공유객체)
 class DataBox {
 	private String data;
-	
+
 	// data가 null이 아닐때 data값을 반환하는 메서드
 	public synchronized String getData() {
-		if(this.data == null) {
+		if (this.data == null) {
 			try {
 				System.out.println(Thread.currentThread().getName() + "wait() 호출");
 				wait();
-			}catch(InterruptedException ex) {
+			} catch (InterruptedException ex) {
 				ex.printStackTrace();
 			}
 		}
 		String returnData = this.data;
 		System.out.println("읽어온 데이터 : " + returnData);
 		this.data = null;
-		System.out.println(Thread.currentThread().getName()+"notify() 호출");
+		System.out.println(Thread.currentThread().getName() + "notify() 호출");
 		notify();
-		
+
 		return returnData;
 	}
 
@@ -57,15 +57,15 @@ class DataBox {
 // 데이터를 세팅만 하는 스레드
 class ProducerThread extends Thread {
 	private DataBox dataBox;
-	
+
 	public ProducerThread(DataBox dataBox) {
 		super("ProducerThread");
 		this.dataBox = dataBox;
 	}
-	
+
 	@Override
 	public void run() {
-		for(int i=1; i<=5; i++) {
+		for (int i = 1; i <= 5; i++) {
 			String data = "Data-" + i;
 			System.out.println("dataBox.setData(" + data + ")호출");
 			dataBox.setData(data);
@@ -81,11 +81,12 @@ class ConsumerThread extends Thread {
 		super("ConsumerThread");
 		this.dataBox = dataBox;
 	}
+
 	@Override
 	public void run() {
-		for(int i=1; i<=5; i++) {
+		for (int i = 1; i <= 5; i++) {
 			String data = dataBox.getData();
-			System.out.println("dataBox.getData() : " +data);
+			System.out.println("dataBox.getData() : " + data);
 		}
 	}
 }
