4ffeb6d1bb3aef9677d41a35ea7e96ec6446e0ff
hayelee <ezezezi94@gmail.com>
2022-10-14 21:39:50 +0900

    20221014 HighJava

---
+++ MyBatisTest/src/kr/or/ddit/member/dao/MemberDaoImpl.java
@@ -0,0 +1,247 @@
+package kr.or.ddit.member.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import kr.or.ddit.member.vo.MemberVO;
+import kr.or.ddit.util.JDBCUtil3;
+
+public class MemberDaoImpl implements IMemberDao {
+	
+	private static IMemberDao memDao; 
+	
+	private Connection conn;
+	private Statement stmt;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	
+	private MemberDaoImpl() {
+		
+	}
+	
+	public static IMemberDao getInstance() {
+		if(memDao == null) {
+			memDao = new MemberDaoImpl();
+		}
+		
+		return memDao;
+	}
+	
+	@Override
+	public int insertMember(MemberVO mv) {
+		
+		int cnt = 0;
+		
+		try {
+			
+			conn = JDBCUtil3.getConnection(); // 커넥션 가져오기
+			
+			String sql = " insert into mymember " 
+			+ " (MEM_ID, MEM_NAME, MEM_TEL, MEM_ADDR, REG_DT)"
+			+ " values (?, ?, ?, ?, sysdate)"; 
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, mv.getMemId());
+			pstmt.setString(2, mv.getMemName());
+			pstmt.setString(3, mv.getMemTel());
+			pstmt.setString(4, mv.getMemAddr());
+			
+			cnt = pstmt.executeUpdate();
+			
+			
+		}catch(SQLException ex){
+			ex.printStackTrace();
+		}finally {
+			JDBCUtil3.close(conn, stmt, pstmt, rs);
+		}
+		return cnt;
+	}
+
+	@Override
+	public boolean checkMember(String memID) {
+		
+		boolean exist = false;
+		
+		try {
+			conn = JDBCUtil3.getConnection();
+			
+			String sql = "select count(*) as cnt " 
+			+ " from mymember " 
+			+ " where mem_id = ? ";
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, memID);
+			
+			rs = pstmt.executeQuery();
+			
+			int cnt = 0;
+			
+			while(rs.next()) { // 한 번 도니까 while 안쓰고 if 써도 됨
+				cnt = rs.getInt("cnt");
+			}
+			
+			if(cnt > 0) {
+				exist = true;
+			}
+			
+		}catch(SQLException ex) {
+			ex.printStackTrace();
+		}finally {
+			JDBCUtil3.close(conn, stmt, pstmt, rs);
+		}
+		return exist;
+	}
+
+	@Override
+	public int updateMember(MemberVO mv) {
+		
+		int cnt = 0;
+		
+		try {
+			conn = JDBCUtil3.getConnection();
+			
+			String sql = " update mymember" 
+					     + " set mem_name = ?,"  
+					     + "     mem_tel = ?,"  
+					     + "     mem_addr = ?" 
+					     + " where mem_id = ?";
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, mv.getMemName());
+			pstmt.setString(2, mv.getMemTel());
+			pstmt.setString(3, mv.getMemAddr());
+			pstmt.setString(4, mv.getMemId());
+			
+			cnt = pstmt.executeUpdate();
+			
+		}catch(SQLException ex) {
+			ex.printStackTrace();
+		}finally {
+			JDBCUtil3.close(conn, stmt, pstmt, rs);
+		}
+		return cnt;
+	}
+
+	@Override
+	public int deleteMember(String memId) {
+	
+		int cnt = 0;
+		
+		try {
+			conn = JDBCUtil3.getConnection();
+			
+			String sql = "delete from mymember where mem_id = ?";
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, memId);
+			
+			cnt = pstmt.executeUpdate();
+			
+		}catch(SQLException ex) {
+			ex.printStackTrace();
+		}finally {
+			JDBCUtil3.close(conn, stmt, pstmt, rs);
+		}
+		return cnt;
+	}
+
+	@Override
+	public List<MemberVO> selectAllMember() {
+		
+		List<MemberVO> memList = new ArrayList<MemberVO>();
+		
+		try {
+			
+			conn = JDBCUtil3.getConnection();
+			
+			String sql = "select * from mymember";
+			
+			stmt = conn.createStatement();
+			
+			rs = stmt.executeQuery(sql);
+			
+			while(rs.next()) {
+				
+				MemberVO mv = new MemberVO();
+				mv.setMemId(rs.getString("mem_id"));
+				mv.setMemName(rs.getString("mem_name"));
+				mv.setMemTel(rs.getString("mem_tel"));
+				mv.setMemAddr(rs.getString("mem_addr"));
+				
+				memList.add(mv); // List에 MemberVO객체 담기
+			}
+			
+		}catch(SQLException ex) {
+			ex.printStackTrace();
+		}finally {
+			JDBCUtil3.close(conn, stmt, pstmt, rs);
+		}
+		
+		return memList;
+	}
+	
+	@Override
+	public List<MemberVO> searchMember(MemberVO mv){
+		List<MemberVO> memList = new ArrayList<MemberVO>();
+		
+		try {
+			conn = JDBCUtil3.getConnection();
+			
+			String sql = "select * from mymember where 1=1"; // and를 붙일지 말지 고민하기 싫어서 1=1 써줌
+			if(mv.getMemId() != null && !mv.getMemId().equals("")) { // 사용자가 회원아이디를  입력 했다는 뜻 
+				sql += " and mem_id = ?";
+			}
+			if(mv.getMemName() != null && !mv.getMemName().equals("")) { // 사용자가 회원이름을 입력 했다는 뜻 
+				sql += " and mem_name = ?";
+			}
+			if(mv.getMemTel() != null && !mv.getMemTel().equals("")) { // 사용자가 회원전화번호를  입력 했다는 뜻 
+				sql += " and mem_tel = ?";
+			}
+			if(mv.getMemAddr() != null && !mv.getMemAddr().equals("")) { // 사용자가 회원주소를  입력 했다는 뜻 
+				sql += " and mem_addr like '%' || ? || '%'";
+			}
+			
+			pstmt = conn.prepareStatement(sql);
+			
+			int index = 1; 
+			if(mv.getMemId() != null && !mv.getMemId().equals("")) { 
+				pstmt.setString(index++, mv.getMemId());
+			}
+			if(mv.getMemName() != null && !mv.getMemName().equals("")) { 
+				pstmt.setString(index++, mv.getMemName());
+			}
+			if(mv.getMemTel() != null && !mv.getMemTel().equals("")) { 
+				pstmt.setString(index++, mv.getMemTel());
+			}
+			if(mv.getMemAddr() != null && !mv.getMemAddr().equals("")) {  
+				pstmt.setString(index++, mv.getMemAddr());
+			}
+			
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				
+				MemberVO mv2 = new MemberVO();
+				mv2.setMemId(rs.getString("mem_id"));
+				mv2.setMemName(rs.getString("mem_name"));
+				mv2.setMemTel(rs.getString("mem_tel"));
+				mv2.setMemAddr(rs.getString("mem_addr"));
+				
+				memList.add(mv2); // List에 MemberVO객체 담기
+			}
+			
+		}catch(SQLException ex) {
+			ex.printStackTrace();
+		}finally {
+			JDBCUtil3.close(conn, stmt, pstmt, rs);
+		}
+		return memList;
+	}
+
+}
