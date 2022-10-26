d390cee754431077bc130a695db1851643c9629d
hayelee <ezezezi94@gmail.com>
2022-10-26 19:09:59 +0900

    20221026 HighJava

--- ServletTest/src/kr/or/ddit/basic/T01ServletLifeCycle.java
+++ ServletTest/src/kr/or/ddit/basic/T01ServletLifeCycle.java
@@ -14,6 +14,9 @@ public class T01ServletLifeCycle extends HttpServlet {
 	 * 서블릿이란?
 	 * 
 	 * 컨테이너(서블릿 엔진)에 의해 관리되는 자바기반 웹컴포넌트로서, 동적인 웹 컨텐츠의 생성을 가능하게 해준다.
+	 * 
+	 * 생성자와 init메서드는 한번만 호출됨.
+	 * init은 초기화작업이 필요한 작업은 한번만 호출되야하니까 init안에 구현하면 좋음
 	 */
 	public T01ServletLifeCycle() {
 		// 생성자
@@ -37,6 +40,8 @@ public class T01ServletLifeCycle extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// 메서드 방식이 get인 경우 호출됨
 		System.out.println("doGet() 호출됨.");
+		
+		throw new ServletException("서블릿에서 난리가 났어요~~~!!!!");
 	}
 
 	@Override
